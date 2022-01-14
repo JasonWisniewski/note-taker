c89b15659a460db43b03a3a6d289a25479dcc65d
Jason Wisniewski <jwizinpa95@gmail.com>
2022-01-13 18:53:36 -0700

    project complete still need final touches

--- routes/apiRoutes/notes.js
+++ routes/apiRoutes/notes.js
@@ -3,6 +3,8 @@ const notes = require('../../db/db.json');
 const fs = require('fs');
 const { stringify } = require('querystring');
 const { response } = require('express');
+const uniqid = require('uniqid');
+const { append } = require('vary');
 
 router.get('/notes', (req, res) => {
   res.json(notes);
@@ -10,17 +12,21 @@ router.get('/notes', (req, res) => {
 })
 
 router.post('/notes', (req,res) => {
-  newNotes = req.body
-  const data = JSON.parse(fs.readFileSync('./db/db.json', 'utf-8')); 
-  console.log(data);
+  let newNotes = req.body
+  newNotes.id = uniqid();
+  // console.log(data);
   console.log(newNotes);
-  data.push(newNotes);
-  fs.writeFileSync('./db/db.json', JSON.stringify(data), function(error){
+  notes.push(newNotes);
+  fs.writeFileSync('./db/db.json', JSON.stringify(notes), function(error){
     if(error){
     console.error(error)};
   })
   // tells us to send data back to front end
-  res.json(newNotes);
+  res.end();
+})
+
+app.delete('/notes:id',(req,res) => {
+  
 })
 
 module.exports = router;
